ffc2840c105c49d129e588be42175d640195072a
Hodakov <teeror93@gmail.com>
2023-09-27 21:25:05 +0400

    REF Правки от Семена из Пул реквеста

--- app/models/post.py
+++ app/models/post.py
@@ -4,9 +4,9 @@ from app.extensions import db
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    ad_number = db.Column(db.String(15), unique=True, nullable=False)
+    ad_id = db.Column(db.String(15), unique=True, nullable=False)
     ad_datetime = db.Column(db.DateTime, nullable=False)
-    url_on_foto = db.Column(db.String, unique=True, nullable=True)
+    image_url = db.Column(db.String, unique=True, nullable=True)
     address = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Integer, nullable=True)
-    Description = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=False)
