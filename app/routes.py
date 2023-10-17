7bb38431f38d0f9666c8008ce071f2aaaae73a73
Valery <91792831+hodakoov@users.noreply.github.com>
2023-10-17 20:23:02 +0400

    ADD. Подробное отображение объявления (#7)

--- app/routes.py
+++ app/routes.py
@@ -8,5 +8,12 @@ bp = Blueprint('index', __name__)
 @bp.route('/')
 def index():
     page = request.args.get('page', 1, type=int)
-    pagination = Post.query.paginate(page=page, per_page=6)
+    pagination = Post.query.paginate(page=page, per_page=8)
     return render_template('index.html', pagination=pagination)
+
+
+@bp.route('/<ad_id>')
+def detail_ad(ad_id: int):
+    post = Post.query.filter_by(ad_id=ad_id).first_or_404()
+    images = post.image_url.split(' ')
+    return render_template('detail_page.html', post=post, images=images)
