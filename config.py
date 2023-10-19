8edc9c74d762db0db673ded1356de65ea1a267d3
LuSP19 <48345486+LuSP19@users.noreply.github.com>
2023-10-19 22:35:28 +0500

    ADD. Добавил скрапер объявлений (#4)

--- config.py
+++ config.py
@@ -7,3 +7,7 @@ class Config:
     # URI используемая для подключения к базе данных
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SCRAPER_INITIAL_URL = 'https://www.avito.ru/moskva/telefony/mobilnye_telefony/apple-ASgBAgICAkS0wA3OqzmwwQ2I_Dc?cd=1'  # noqa: E501
+    SCRAPER_PAGE_LIMIT = 1
+    SCRAPER_DELAY_LIMIT = 20
