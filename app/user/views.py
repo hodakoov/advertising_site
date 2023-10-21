0788535cc503903f37776e9e68a287c84faf14cf
LuSP19 <semyon.starykh@gmail.com>
2023-10-21 12:58:33 +0500

    ADD. Логин и регистрация

---
+++ app/user/views.py
@@ -0,0 +1,64 @@
+from flask import Blueprint, flash, redirect, render_template, url_for
+from flask_login import (
+    LoginManager, current_user, login_required, login_user, logout_user
+)
+
+from app.extensions import db
+from app.user.forms import LoginForm, RegistrationForm
+from app.user.models import User
+
+blueprint = Blueprint('user', __name__, url_prefix='/users')
+
+
+@blueprint.route('/login')
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index.index'))
+    title = 'Авторизация'
+    login_form = LoginForm()
+    return render_template('user/login.html', page_title=title, form=login_form)
+
+
+@blueprint.route('/process-login', methods=['POST'])
+def process_login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter(User.username == form.username.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            flash('Вы успешно вошли на сайт')
+            return redirect(url_for('index.index'))
+
+    flash('Неправильные имя или пароль')
+    return redirect(url_for('user.login'))
+
+
+@blueprint.route('/logout')
+def logout():
+    logout_user()
+    flash('Вы разлогинились')
+    return redirect(url_for('index.index'))
+
+
+@blueprint.route('/register')
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index.index'))
+    title = 'Регистрация'
+    form = RegistrationForm()
+    return render_template('user/registration.html', page_title=title, form=form)
+
+
+@blueprint.route('/process-reg', methods=['POST'])
+def process_reg():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data, email=form.email.data, role='user')
+        new_user.set_password(form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Вы успешно зарегистрировались!')
+        return redirect(url_for('user.login'))
+    flash('Пожалуйста, исправьте ошибки в форме')
+    return redirect(url_for('user.register'))
